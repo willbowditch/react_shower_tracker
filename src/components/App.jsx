e4767049249d0bfc47e8daba5d58cfb755bf1dea
willbowditch <will@bowdit.ch>
2018-10-31 18:02:36 +0000

    This week - learnt how to use firebase live db support and react router /shower/id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,15 @@
-import React, {Component} from 'react'
-import Table from './Table'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import ShowerOverview from './ShowerOverview'
+import ShowerDetail from './ShowerDetail'
 
-class App extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            showers: [
-                {
-                    'floor': 9,
-                    'status': false
-                },
-                {
-                    'floor': 11,
-                    'status': false
-                },
-                {
-                    'floor': 13,
-                    'status': false
-                },
-                {
-                    'floor': 15,
-                    'status': false
-                }
-            ]
-        }
-
-        this.handleShowerInUse = this.handleShowerInUse.bind(this)
-    }
-
-    handleShowerInUse(e){
-        this.setState((currentState) => {
-            const targetShower = currentState.showers.find(
-                (shower) => shower.floor === e
-            )
-
-            return{
-                showers: currentState.showers.map((shower)=> {
-                    if (shower == targetShower) {
-                        shower.status = !shower.status
-                        return shower
-                    } else {
-                        return shower
-                    }
-                })
-            }
-        })
-    }
-
-
-    render() {
-        const showerData = this.state.showers
-
-        return (
-            <div className="row">
-                <div className="col-sm-8">
-                    <h1>Shower Tracker</h1>
-                    <Table
-                        showerData={showerData}
-                        handleShowerInUse={this.handleShowerInUse}
-                    />
-                </div>
-            </div>
-        )
-    }
-}
-
+const App = () => (
+    <Router>
+        <div>
+            <Route exact path='/' component={ShowerOverview}/>
+            <Route path='/shower/:id' component={ShowerDetail}/>
+        </div>
+    </Router>
+)
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { hydrate } from 'react-dom'
 import App from './components/App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 hydrate(
     <App />,
